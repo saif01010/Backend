e58d56b65d8b2740b8b45ab3f8a6164f7a1ef0e5
saif01010 <saiful.12006518@lpu.in>
2024-02-06 22:40:24 +0530

    solved some sone debug

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,4 +1,4 @@
-class ApiError extends error{
+class ApiError extends Error{
     constructor(statusCode,
         message="Something went wrong",
         errors=[],
@@ -20,4 +20,4 @@ class ApiError extends error{
     }
 }
 
-export default ApiError;
+export  {ApiError};
