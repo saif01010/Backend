62d82b0d25ecc9d4d2943b7be350d1b9c9fa0c3c
saif01010 <saiful.12006518@lpu.in>
2024-03-12 19:47:51 +0530

    Remove unnecessary code in app.js

--- src/app.js
+++ src/app.js
@@ -3,11 +3,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import path from "path";
 
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 const app = express();
-app.use(express.static(path.join(__dirname, "views")));
 
 app.use(cors({
     origin:process.env.CLIENT_URL,
