7fb35166df6ab10c27299db0e115c324b7e8d2c0
saif01010 <saiful.12006518@lpu.in>
2024-03-14 20:24:34 +0530

    Add CORS methods and Vercel route configuration

--- src/app.js
+++ src/app.js
@@ -9,7 +9,8 @@ const app = express();
 
 app.use(cors({
     origin:process.env.CLIENT_URL,
-    credentials:true
+    credentials:true,
+    methods:"GET,POST,PUT,DELETE",
 }));
 
 // app.use(session({
